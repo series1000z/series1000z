1e64568be8f83f907bf509abcd1ecd56e22a37e9
series1000z <t0133188@gmail.com>
2026-06-19 05:22:19 +0700

    Update anime-data.js

--- anime-data.js
+++ anime-data.js
@@ -2,7 +2,7 @@ const animeList = [
 
      {
         "title": "เด็กแฝดราชามังกร ซับไทย",
-        "img": "https://drive.google.com/u/0/drive-viewer/AKGpihaG7ISKwTwv6uevGDR-6W-PWB5XyT86VOPy-fDV3r4yvNfElbwW9bMDQvTI4ajJuuNn2dAqkzDg_8TAOjlX3cayHHdq-E6hjM0=s1600-rw-v1?auditContext=forDisplay",
+        "img": "https://drive.google.com/file/d/1iGWsrr08oWLjhEKMavAAKDf-DAECra7P/view",
         "type": "ซับไทย",
         "link": "https://series1000z.com/Dragon-King-Twin-Children.html"
     },
