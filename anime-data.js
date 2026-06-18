c55c52202cc86f83a48231c1126ed78be84cef5a
series1000z <t0133188@gmail.com>
2026-06-19 05:19:20 +0700

    Update anime-data.js

--- anime-data.js
+++ anime-data.js
@@ -9,7 +9,7 @@ const animeList = [
 
      {
         "title": "ยอดภารโรงพันธุ์มหาประลัย พากย์ไทย",
-        "img": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgCjMKHpi1VostzXCzhAe-JwR0XpE-KsUHW8GggH_Gf-Iwc6ZjykiSe6yoSZykwROm2An751qu6NDRfx7YosYhONqvtgJnJbI30NoydUONqvSXQ-ov6k8NFjRFKIHWWn5LosaepTDaaLvPlfzIwNmkYTuDiX8olhW8cU0ODGb3n3jcM2eXpN1CbScgEZg/s320/messageImage_1781735029860.jpg",
+        "img": "https://drive.google.com/u/0/drive-viewer/AKGpihaG7ISKwTwv6uevGDR-6W-PWB5XyT86VOPy-fDV3r4yvNfElbwW9bMDQvTI4ajJuuNn2dAqkzDg_8TAOjlX3cayHHdq-E6hjM0=s1600-rw-v1?auditContext=forDisplay",
         "type": "พากย์ไทย",
         "link": "https://series1000z.com/The-Ultimate-Janitor.html"
     },
