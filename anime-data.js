ee4ed3568378bfdd6f58206d62b34ca378ca00bb
series1000z <t0133188@gmail.com>
2026-06-19 05:34:14 +0700

    Update anime-data.js

--- anime-data.js
+++ anime-data.js
@@ -2,14 +2,14 @@ const animeList = [
 
      {
         "title": "เด็กแฝดราชามังกร ซับไทย",
-        "img": "https://lh3.googleusercontent.com/d/1iGWsrr08oWLjhEKMavAAKDf-DAECra7P=s220?authuser=0",
+        "img": "https://drive.usercontent.google.com/download?id=1iGWsrr08oWLjhEKMavAAKDf-DAECra7P&export=view&authuser=0",
         "type": "ซับไทย",
         "link": "https://series1000z.com/Dragon-King-Twin-Children.html"
     },
 
      {
         "title": "ยอดภารโรงพันธุ์มหาประลัย พากย์ไทย",
-        "img": "https://lh3.googleusercontent.com/d/1iGWsrr08oWLjhEKMavAAKDf-DAECra7P=s220?authuser=0",
+        "img": "https://drive.usercontent.google.com/download?id=1iGWsrr08oWLjhEKMavAAKDf-DAECra7P&export=view&authuser=0",
         "type": "พากย์ไทย",
         "link": "https://series1000z.com/The-Ultimate-Janitor.html"
     },
