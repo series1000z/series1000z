8ed593e492a3173e243320e823703d07f3df817d
series1000z <t0133188@gmail.com>
2026-06-20 05:05:16 +0700

    Create anime-data-animethai.js

---
+++ anime-data-animethai.js
@@ -0,0 +1,22 @@
+const animeList = [
+
+     {
+        "title": "เด็กแฝดราชามังกร ซับไทย",
+        "img": "https://lh3.googleusercontent.com/d/1iGWsrr08oWLjhEKMavAAKDf-DAECra7P=s1000",
+        "type": "ซับไทย",
+        "link": "https://series1000z.com/Dragon-King-Twin-Children.html"
+    },
+
+     {
+        "title": "ยอดภารโรงพันธุ์มหาประลัย พากย์ไทย",
+        "img": "https://lh3.googleusercontent.com/d/1EqnvR-IttAapjhsTsFNxsw0dgbnxFqLe=s1000",
+        "type": "พากย์ไทย",
+        "link": "https://series1000z.com/The-Ultimate-Janitor.html"
+    },
+
+
+  
+   
+
+  // เพิ่มรายการอนิเมะได้เรื่อย ๆ
+];
