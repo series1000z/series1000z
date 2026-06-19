c8df54a84df2e34f7cebdd450dd9d95b90394e10
series1000z <t0133188@gmail.com>
2026-06-20 05:23:09 +0700

    Update anime-data-animethai.js

--- anime-data-animethai.js
+++ anime-data-animethai.js
@@ -3,14 +3,14 @@ const animeList = [
      {
         "title": "เด็กแฝดราชามังกร ซับไทย",
         "img": "https://lh3.googleusercontent.com/d/1iGWsrr08oWLjhEKMavAAKDf-DAECra7P=s1000",
-        "type": "ซับไทย",
+        "type": "อนิเมะซับไทย",
         "link": "https://series1000z.com/Dragon-King-Twin-Children.html"
     },
 
      {
         "title": "ยอดภารโรงพันธุ์มหาประลัย พากย์ไทย",
         "img": "https://lh3.googleusercontent.com/d/1EqnvR-IttAapjhsTsFNxsw0dgbnxFqLe=s1000",
-        "type": "พากย์ไทย",
+        "type": "อนิเมะพากย์ไทย",
         "link": "https://series1000z.com/The-Ultimate-Janitor.html"
     },
 
