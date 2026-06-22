ca2d00cf67823998d78b57e7ad42b67e000ae058
series1000z <t0133188@gmail.com>
2026-06-23 02:26:33 +0700

    Update anime-data-sub.js

--- anime-data-sub.js
+++ anime-data-sub.js
@@ -1,5 +1,45 @@
 const animeList = [
 
+                 {
+        "title": "ราชาผีคืนชีพในร่างน้องชายแฟน พากย์ไทย",
+        "img": "https://lh3.googleusercontent.com/d/11JypChpy1HgErRPvOBEJ2xzF7sMvKQxT=s1000",
+        "type": "อนิเมะพากย์ไทย",
+        "link": "https://series1000z.com/The-Ghost-King-is-resurrected.html"
+    },
+
+           {
+        "title": "นักเรียนยากจนที่รวยที่สุด พากย์ไทย",
+        "img": "https://lh3.googleusercontent.com/d/1Q01w4iZtNAcgRv1E-5xyGkk3T3qQfAuG=s1000",
+        "type": "พากย์ไทย",
+        "link": "https://series1000z.com/The-richest-poor-student.html"
+    },
+             {
+        "title": "ราชินี ไร้บัลลังก์ ซับไทย",
+        "img": "https://lh3.googleusercontent.com/d/18EJRXgLdLccI_oKAyNfLxDi7-CxefzcQ=s1000",
+        "type": "อนิเมะซับไทย",
+        "link": "https://series1000z.com/The-Queen-Without-a-Throne.html"
+    },
+
+      {
+        "title": "เจ้าสาวจำยอมของราชามังกร พากย์ไทย",
+        "img": "https://lh3.googleusercontent.com/d/1jH0kEgnS8ArCiMoIOXLWs3bAoycaWtrO=s1000",
+        "type": "อนิเมะพากย์ไทย",
+        "link": "https://series1000z.com/The-Dragon-King-Reluctant-Bride.html"
+    },
+             {
+        "title": "นางร้ายผู้กู้โลก ซับไทย",
+        "img": "https://lh3.googleusercontent.com/d/1JakkQougGmic6p4wJhIIjtUuCfwVezw9=s1000",
+        "type": "อนิเมะซับไทย",
+        "link": "https://series1000z.com/The-Villainess-Who-Saved-the-World.html"
+    },
+
+        {
+        "title": "แกล้งเดท กับคาวบอยอันตราย พากย์ไทย",
+        "img": "https://lh3.googleusercontent.com/d/1Yfa36XeeWWc_PdkQFPU5m98yrB21LiY3=s1000",
+        "type": "อนิเมะพากย์ไทย",
+        "link": "https://series1000z.com/Pretend-a-Date-with-a-Dangerous-Cowboy.html"
+    },
+
           {
         "title": "พี่สาวฉันเป็นเจ้าแม่มาเฟีย พากย์ไทย",
         "img": "https://lh3.googleusercontent.com/d/1aSL-H66i1L7f5UA5H3euZjZFXwe8Ghsw=s1000",
